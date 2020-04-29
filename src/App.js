12273f6c96e3432d50feec7657342276d3d90b46
sophie <sneuville59@gmail.com>
2020-04-29 11:24:48 +0200

    nettoyage des fichiers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from "react";
-import Wilder from "./components/Wilder";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hi there !</h1>
-      <Wilder />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;

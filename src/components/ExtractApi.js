7b0b066ee5c74bcd0afb892f704dcabfb593e044
sophie <sneuville59@gmail.com>
2020-04-29 21:19:00 +0200

    modification extraction de la video et titre

--- src/components/ExtractApi.js
+++ src/components/ExtractApi.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function ExtractApi() {
-  const [country, setCountry] = useState("FR"); //va définir le pays dans lequel on va voir les webcams
+  const [country, setCountry] = useState("EG"); //va définir le pays dans lequel on va voir les webcams
   const [webcam, setWebcam] = useState([]); //enregistre dans un tableau les id des webcams
   const [video, setVideo] = useState(false); //enregistre dans un tableau les url liens des webcams
 
@@ -40,12 +40,8 @@ export default function ExtractApi() {
         `https://api.windy.com/api/webcams/v2/list/webcam=${webcam.id}?show=webcams:image,location,player&key=q7WhxCHqIIMwge4tYv97cddN2NWHHb2p`
       )
       .then((data) => {
-        //console.log(data);
-        //console.log(data.data.result.webcams[0].image.current.preview);
-        const preview = data.data.result.webcams[0];
-        const player = data.data.result.webcams[0];
-        console.log(player, preview);
-        setVideo(player);
+        console.log(data.data.result.webcams[0]);
+        setVideo(data.data.result.webcams[0]);
       });
   };
   //console.log(video);
@@ -53,7 +49,7 @@ export default function ExtractApi() {
   return (
     <div>
       <div>{video && <iframe alt="" src={video.player.lifetime.embed} />}</div>
-      <div>{video && <img alt="" src={video.image.current.preview} />}</div>
+      <div>{video && <p>{video.title}</p>}</div>
     </div>
   );
 }

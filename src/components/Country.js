ebfc59d6ab0ce4832b0fd5ec78b5771d25e69a55
Ghazi-1 <gh.maes@gmail.com>
2020-04-30 11:38:18 +0200

    css Countrypage

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import "./Country.scss";
 
 export default function Country() {
   const [webcam, setWebcam] = useState({});
@@ -38,11 +39,20 @@ export default function Country() {
   };
 
   return (
-    <div>
-      {info && <iframe alt="" src={info.player.lifetime.embed} />}
-      <p>
-        Hello {info && info.location.city}, {info && info.location.country} !
+    <div className="Videopage">
+      <p className="text">
+        {info && info.location.city},{" "}
+        <span>{info && info.location.country} !</span>
       </p>
+      {info && (
+        <iframe
+          width="550"
+          height="450"
+          alt=""
+          src={info.player.lifetime.embed}
+        />
+      )}
+      <img className="logo-videopage" src="/logo.png" alt="logo" />
     </div>
   );
 }

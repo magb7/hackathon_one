59cde145ca62b5b0cca9a46400b3b14ec9c3fddb
sophie <sneuville59@gmail.com>
2020-04-30 12:01:52 +0200

    changement des destinations affichées pour avoir des plages

--- src/components/Country.js
+++ src/components/Country.js
@@ -17,7 +17,7 @@ export default function Country() {
   const getCountry = () => {
     axios
       .get(
-        `https://api.windy.com/api/webcams/v2/list/country=${country}?key=q7WhxCHqIIMwge4tYv97cddN2NWHHb2p`
+        `https://api.windy.com/api/webcams/v2/list/country=${country}/category=beach?key=q7WhxCHqIIMwge4tYv97cddN2NWHHb2p`
       )
       .then((data) => {
         setWebcam(data.data.result.webcams[0].id);
